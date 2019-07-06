2701b8cd85ee74acb6012b2ce25cbb0744c8664c
Messias Souza <44982001+26messias26@users.noreply.github.com>
2019-07-06 13:31:46 -0300

    Add files via upload

--- testdePesquisa/js/main.js
+++ testdePesquisa/js/main.js
@@ -1,23 +1,26 @@
+
+// --- FUNÇÃO DE PESQUISA --- //
 function bookSearch(){
     let dado = document.getElementById('search').value
     document.getElementById('results').innerHTML = ""
-    console.log(dado)
-
+    // --- SOLICITA OS DADOS --- //
     $.ajax({
         url: "https://www.googleapis.com/books/v1/volumes?q=" + dado,
         dataType: "json",
+        type:'GET',
+        // CASO ENCONTRADO CRIARA O CARD
         success: function(data){
+            console.log(data)
             for(i = 0; i<data.items.length; i++){
                 results.innerHTML += `
-                <div>
+                <div class="card_book">
                 <h5> ${data.items[i].volumeInfo.title} </h5>
                 ${data.items[i].volumeInfo.authors}
                 
                 </div>`
             }
-        },
-
-        type:'GET'
+        }
+        
 
     })
 
